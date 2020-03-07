45e94771d501eb63c28d580b9837b8476e5bd179
animesh <animuz111@gmail.com>
2020-03-07 13:08:40 +0530

    [backed] Update Pagure backend
    This commit makes the below updates to pagure backend:
    1. Removed assignee_data and user_data from response
    2. Fixed origin bug inn case of namespcae
    3. Doc fixes
    4. Added ssl_verify argument
    5. Remove base_url argument
    6. Updated api-token argument of PagureCommand
    
    Signed-off-by: animesh <animuz111@gmail.com>

--- perceval/backends/core/pagure.py
+++ perceval/backends/core/pagure.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2015-2019 Bitergia
+# Copyright (C) 2015-2020 Bitergia
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -16,15 +16,13 @@
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
 # Authors:
-#     Valerio Cosentino <valcos@bitergia.com>
+#     Animesh Kumar <animuz111@gmail.com>
 #
 
 import json
 import logging
-import datetime
 import requests
-from grimoirelab_toolkit.datetime import (datetime_to_utc,
-                                          datetime_utcnow,
+from grimoirelab_toolkit.datetime import (datetime_utcnow,
                                           str_to_datetime)
 from grimoirelab_toolkit.uris import urijoin
 
@@ -41,18 +39,12 @@ CATEGORY_ISSUE = "issue"
 PAGURE_URL = "https://pagure.io/"
 PAGURE_API_URL = "https://pagure.io/api/0"
 
-# Range before sleeping until rate limit reset
-MIN_RATE_LIMIT = 10
-MAX_RATE_LIMIT = 500
-
 MAX_CATEGORY_ITEMS_PER_PAGE = 100
 PER_PAGE = 100
 
 # Default sleep time and retries to deal with connection/server problems
 DEFAULT_SLEEP_TIME = 1
 MAX_RETRIES = 5
-
-TARGET_ISSUE_FIELDS = ['user', 'assignee']
 
 logger = logging.getLogger(__name__)
 
@@ -67,55 +59,40 @@ class Pagure(Backend):
     :param namespace: Pagure namespace
     :param repository: Pagure repository; in case the repository is within a namespace
     :param api_token: Pagure API token to access the API
-    :param base_url: when no value is set the backend will be fetch
-     the data from the Pagure public site.
     :param tag: label used to mark the data
     :param archive: archive to store/retrieve items
-    :param sleep_for_rate: sleep until rate limit is reset
-    :param min_rate_to_sleep: minimum rate needed to sleep until
-         it will be reset
     :param max_retries: number of max retries to a data source
         before raising a RetryError exception
     :param max_items: max number of category items (e.g., issues,
         pull requests) per query
     :param sleep_time: time to sleep in case
         of connection problems
+    :param ssl_verify: enable/disable SSL verification
     """
-    version = '0.29'
+    version = '0.1.0'
 
     CATEGORIES = [CATEGORY_ISSUE]
-    CLASSIFIED_FIELDS = [
-        ['user_data'],
-        ['assignee_data']
-    ]
 
     def __init__(self, namespace=None, repository=None,
-                 api_token=None, base_url=None,
+                 api_token=None,
                  tag=None, archive=None,
-                 sleep_for_rate=False, min_rate_to_sleep=MIN_RATE_LIMIT,
                  max_retries=MAX_RETRIES, sleep_time=DEFAULT_SLEEP_TIME,
-                 max_items=MAX_CATEGORY_ITEMS_PER_PAGE):
-        if api_token is None:
-            api_token = []
-        origin = base_url if base_url else PAGURE_URL
-        origin = urijoin(origin, namespace, repository)
+                 max_items=MAX_CATEGORY_ITEMS_PER_PAGE, ssl_verify=True):
+        origin = PAGURE_URL
 
-        super().__init__(origin, tag=tag, archive=archive)
+        # In case repository is under a namespace add the namespace as well to the origin
+        origin = urijoin(origin, namespace, repository) if namespace else urijoin(origin, repository)
+
+        super().__init__(origin, tag=tag, archive=archive, ssl_verify=ssl_verify)
 
         self.namespace = namespace
         self.repository = repository
         self.api_token = api_token
-        self.base_url = base_url
-
-        self.sleep_for_rate = sleep_for_rate
-        self.min_rate_to_sleep = min_rate_to_sleep
         self.max_retries = max_retries
         self.sleep_time = sleep_time
         self.max_items = max_items
 
         self.client = None
-        self.exclude_user_data = False
-        self._users = {}  # internal users cache
 
     def search_fields(self, item):
         """Add search fields to an item.
@@ -148,11 +125,6 @@ class Pagure(Backend):
 
         :returns: a generator of issues
         """
-        self.exclude_user_data = filter_classified
-
-        if self.exclude_user_data:
-            logger.info("Excluding user data. Personal user information won't be collected from the API.")
-
         if not from_date:
             from_date = DEFAULT_DATETIME
 
@@ -218,13 +190,10 @@ class Pagure(Backend):
 
         :returns: a UNIX timestamp
         """
-        if "forks_count" in item:
-            return item['fetched_on']
-        else:
-            ts = int(item['last_updated'])
-            ts = datetime.fromtimestamp(ts).timestamp()
+        ts = int(item['last_updated'])
+        ts = datetime.fromtimestamp(ts).timestamp()
 
-            return ts
+        return ts
 
     @staticmethod
     def metadata_category(item):
@@ -233,7 +202,6 @@ class Pagure(Backend):
         This backend generates one type of item which is
         'issue'.
         """
-
         category = CATEGORY_ISSUE
 
         return category
@@ -241,10 +209,9 @@ class Pagure(Backend):
     def _init_client(self, from_archive=False):
         """Init client"""
 
-        return PagureClient(self.namespace, self.repository, self.api_token, self.base_url,
-                            self.sleep_for_rate, self.min_rate_to_sleep,
+        return PagureClient(self.namespace, self.repository, self.api_token,
                             self.sleep_time, self.max_retries, self.max_items,
-                            self.archive, from_archive)
+                            self.archive, from_archive, self.ssl_verify)
 
     def __fetch_issues(self, from_date, to_date):
         """Fetch the issues
@@ -253,7 +220,6 @@ class Pagure(Backend):
 
         :returns: an issue object
         """
-
         issues_groups = self.client.issues(from_date=from_date)
 
         for raw_issues in issues_groups:
@@ -264,43 +230,7 @@ class Pagure(Backend):
                 if int(issue['last_updated']) > str_to_datetime(to_date).timestamp():
                     return
 
-                self.__init_extra_issue_fields(issue)
-                for field in TARGET_ISSUE_FIELDS:
-
-                    if not issue[field]:
-                        continue
-
-                    if field == 'user':
-                        issue[field + '_data'] = self.__get_user(issue[field]['name'])
-                    elif field == 'assignee':
-                        issue[field + '_data'] = self.__get_issue_assignee(issue[field])
-
                 yield issue
-
-    def __get_issue_assignee(self, raw_assignee):
-        """Get issue assignee"""
-        if not raw_assignee:
-            return None
-        assignee = self.__get_user(raw_assignee['name'])
-
-        return assignee
-
-    def __get_user(self, login):
-        """Get user data for the login"""
-
-        if not login or self.exclude_user_data:
-            return None
-
-        user_raw = self.client.user(login)
-        user = json.loads(user_raw)
-
-        return user
-
-    def __init_extra_issue_fields(self, issue):
-        """Add fields to an issue"""
-
-        issue['user_data'] = {}
-        issue['assignee_data'] = {}
 
 
 class PagureClient(HttpClient, RateLimitHandler):
@@ -308,12 +238,7 @@ class PagureClient(HttpClient, RateLimitHandler):
 
     :param namespace: Pagure namespace
     :param repository: Pagure repository; incase the repository is within a namespace
-    :param tokens: Pagure API token to access the API
-    :param base_url: When no value is set the backend will be fetch the data
-        from the Pagure public site.
-    :param sleep_for_rate: sleep until rate limit is reset
-    :param min_rate_to_sleep: minimun rate needed to sleep until
-         it will be reset
+    :param token: Pagure API token to access the API
     :param sleep_time: time to sleep in case
         of connection problems
     :param max_retries: number of max retries to a data source
@@ -322,36 +247,30 @@ class PagureClient(HttpClient, RateLimitHandler):
         pull requests) per query
     :param archive: collect issues already retrieved from an archive
     :param from_archive: it tells whether to write/read the archive
+    :param ssl_verify: enable/disable SSL verification
     """
     EXTRA_STATUS_FORCELIST = [403, 500, 502, 503]
 
-    _users = {}  # users cache
-
-    def __init__(self, namespace, repository, tokens,
-                 base_url=None, sleep_for_rate=False, min_rate_to_sleep=MIN_RATE_LIMIT,
+    def __init__(self, namespace, repository, token,
                  sleep_time=DEFAULT_SLEEP_TIME, max_retries=MAX_RETRIES,
-                 max_items=MAX_CATEGORY_ITEMS_PER_PAGE, archive=None, from_archive=False):
+                 max_items=MAX_CATEGORY_ITEMS_PER_PAGE, archive=None, from_archive=False, ssl_verify=True):
         self.namespace = namespace
         self.repository = repository
-        self.tokens = tokens
-        self.n_tokens = len(self.tokens)
-        self.current_token = None
-        self.last_rate_limit_checked = None
+        self.token = token
         self.max_items = max_items
 
+        # URL to fetch the data from
         base_url = PAGURE_API_URL
 
         super().__init__(base_url, sleep_time=sleep_time, max_retries=max_retries,
                          extra_headers=self._set_extra_headers(),
                          extra_status_forcelist=self.EXTRA_STATUS_FORCELIST,
-                         archive=archive, from_archive=from_archive)
-        super().setup_rate_limit_handler(sleep_for_rate=sleep_for_rate, min_rate_to_sleep=min_rate_to_sleep)
+                         archive=archive, from_archive=from_archive, ssl_verify=ssl_verify)
 
     def calculate_time_to_reset(self):
         """Calculate the seconds to reset the token requests, by obtaining the different
         between the current date and the next date when the token is fully regenerated.
         """
-
         time_to_reset = self.rate_limit_reset_ts - (datetime_utcnow().replace(microsecond=0).timestamp() + 1)
         time_to_reset = 0 if time_to_reset < 0 else time_to_reset
 
@@ -370,7 +289,7 @@ class PagureClient(HttpClient, RateLimitHandler):
         payload = {
             'status': 'all',
             'per_page': self.max_items,
-            'order': 'asc',
+            'order': 'asc'
         }
 
         if from_date:
@@ -379,54 +298,24 @@ class PagureClient(HttpClient, RateLimitHandler):
         path = urijoin("issues")
         return self.fetch_items(path, payload)
 
-    def user(self, name):
-        """Get the user information and update the user cache
-
-        :param name: username of the user
-
-        :returns: a user object
-        """
-        user = None
-
-        if name in self._users:
-            return self._users[name]
-
-        url_user = urijoin(self.base_url, 'user', name)
-
-        logger.debug("Getting info for %s" % url_user)
-
-        r = self.fetch(url_user)
-        user = r.text
-        self._users[name] = user
-
-        return user
-
-    def fetch(self, url, payload=None, headers=None, method=HttpClient.GET, stream=False, auth=True):
+    def fetch(self, url, payload=None, headers=None):
         """Fetch the data from a given URL.
 
         :param url: link to the resource
         :param payload: payload of the request
         :param headers: headers of the request
-        :param method: type of request call (GET or POST)
-        :param stream: defer downloading the response body until the response content is available
         :param auth: auth of the request
 
         :returns a response object
         """
-        if not self.from_archive:
-            self.sleep_for_rate_limit()
-
-        # In case the issue tracker is disabled for a repository
-        # an HTTP 404 response is returned
         try:
-            response = super().fetch(url, payload, headers, method, stream, auth)
-        except requests.exceptions.HTTPError:
-            print("The issue tracker is disabled please enable the feature for the repository")
-            exit(1)
-            return
-
-        if not self.from_archive:
-            self.update_rate_limit(response)
+            response = super().fetch(url, payload, headers)
+        except requests.exceptions.HTTPError as error:
+            if error.response.status_code == 404 and error.response.reason == 'NOT FOUND':
+                logger.warning("The issue tracker is disabled please enable the feature for the repository")
+                return None
+            else:
+                raise error
 
         return response
 
@@ -438,16 +327,17 @@ class PagureClient(HttpClient, RateLimitHandler):
 
         :returns: an item object
         """
-
         page = 0  # current page
         last_page = None  # last page
-        if self.namespace:  # if project is under a namspace
-            url_next = urijoin(self.base_url, self.namespace, self.repository, path)
+        if self.namespace:  # if project is under a namespace
+            url_next = urijoin(PAGURE_API_URL, self.namespace, self.repository, path)
         else:  # if project is created without a namespace
-            url_next = urijoin(self.base_url, self.repository, path)
+            url_next = urijoin(PAGURE_API_URL, self.repository, path)
         logger.debug("Get Pagure paginated items from " + url_next)
 
         response = self.fetch(url_next, payload=payload)
+        if not response:
+            return []
 
         items = response.text
         page += 1
@@ -475,8 +365,8 @@ class PagureClient(HttpClient, RateLimitHandler):
         """Set extra headers for session"""
 
         headers = {}
-        if self.current_token:
-            headers = {'Authorization': "token %s" % self.tokens}
+        if self.token:
+            headers = {'Authorization': "token %s" % self.token}
 
         return headers
 
@@ -493,14 +383,12 @@ class PagureCommand(BackendCommand):
         parser = BackendCommandArgumentParser(cls.BACKEND,
                                               from_date=True,
                                               to_date=True,
-                                              token_auth=False,
-                                              archive=True)
+                                              token_auth=True,
+                                              archive=True,
+                                              ssl_verify=True)
 
         # Pagure authorisation token
         group = parser.parser.add_argument_group('Pagure arguments')
-        group.add_argument('--api-token', dest='api-token',
-                           type=str,
-                           help="Set when using API token")
 
         # Generic client options
         group.add_argument('--max-items', dest='max_items',
@@ -519,10 +407,8 @@ class PagureCommand(BackendCommand):
         # hence API call supports the access based on usecase. e.g.
         # GET /api/0/<repo>/issues
         # GET /api/0/<namespace>/<repo>/issues
-
         parser.parser.add_argument('namespace', nargs='?',
                                    help="Pagure namespace")
         parser.parser.add_argument('repository',
                                    help="Pagure repository; in case the repository is within a namespace")
-
         return parser
